2e8b238002952ae8beada9c1a0a9169882f1ba06
Rubén Prieto García <rubenprietogarcia10@gmail.com>
2025-06-05 03:57:59 +0200

    Auto-commit TFG @ 2025-06-05 03:57:59

--- Recoleccion/dockers/capturar_tcpdump/entrypoint.sh
+++ Recoleccion/dockers/capturar_tcpdump/entrypoint.sh
@@ -5,4 +5,4 @@ PORT=${TCPDUMP_BROADCAST_PORT:-5555}
 IFACE=${CAPTURE_INTERFACE:-ens4}
 
 echo "🟢 tcpdump sirviendo PCAP por ${PORT} desde ${IFACE}"
-exec socat -u TCP-LISTEN:"${PORT}",reuseaddr,fork EXEC:"tcpdump -nn -i ${IFACE} -s0 -U -w -",nofork
+exec socat -u TCP-LISTEN:"${PORT}",reuseaddr,fork EXEC:"tcpdump -nn -i ${IFACE} -s0 -U -w 2>/dev/null",nofork > /dev/null 2>&1
