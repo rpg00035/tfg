3ac3b132024285d32c95a7f705b593ed3da634e9
Rubén Prieto García <rubenprietogarcia10@gmail.com>
2025-06-05 03:59:09 +0200

    Auto-commit TFG @ 2025-06-05 03:59:09

--- Recoleccion/dockers/procesar_argus/entrypoint.sh
+++ Recoleccion/dockers/procesar_argus/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-TCPDUMP_HOST${TCPDUMP_HOST:-127.0.0.1}
+TCPDUMP_HOST=${TCPDUMP_HOST:-127.0.0.1}
 TCPDUMP_PORT=${TCPDUMP_BROADCAST_PORT:-5555}
 ARGUS_PORT=${ARGUS_PORT:-561}
 
@@ -9,4 +9,4 @@ ARGUS_PORT=${ARGUS_PORT:-561}
 
 echo "[Argus] conectado a ${TCP_HOST}:${TCP_PORT}"
 
-socat -u TCP:${TCPDUMP_HOST}:${TCPDUMP_PORT} - | argus -r - -P ${ARGUS_PORT} -m -w -
+socat -u TCP:${TCPDUMP_HOST}:${TCPDUMP_PORT} - | argus -r - -P ${ARGUS_PORT} -m -w - > /dev/null 2>&1
